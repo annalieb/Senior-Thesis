f0622cd166b25e1a46f529de9bbc4fda4b0d93b8
Anna Lieb <annaslieb@gmail.com>
2024-01-06 02:58:10 -0500

    calculated action label f1 scores

--- GPT_label_results/calc_performance.py
+++ GPT_label_results/calc_performance.py
@@ -3,9 +3,10 @@ from sklearn.metrics import f1_score
 import pandas as pd
 
 def get_classwise_f1(): 
-    all_labels = pd.read_csv("actor_preds.csv")
+    all_labels = pd.read_csv("all_preds.csv")
 
     conversions = {
+        # actor
         "<EDUCATIONAL PRACTITIONER>": 0,
         "educational practitioners": 0,
         "<POLITICAL INFLUENCER>": 1,
@@ -14,11 +15,30 @@ def get_classwise_f1():
         "impacted actors": 2,
         "<NONE/OTHER>": 3,
         "none / other": 3,
+        # action
+        "<PROTEST / SPEAKING OUT>": 0, 
+        "protest / speaking out": 0,
+        "<POLICY / LEGAL / ELECTIONS>": 1, 
+        "policy & legal": 1, 
+        "elections": 1,
+        "<THREATS / EXTENT>": 2, 
+        "threats/extent": 2,
+        "<NONE/OTHER>": 3,
+        "none/other": 3, 
+        # action direction
+        # action stance
     }
 
+    ##### ACTOR #####
     actor_true = [conversions[l] for l in all_labels['actor']]
     actor_pred = [conversions[l] for l in all_labels['actor_pred']]
     classwise_f1 = f1_score(actor_true, actor_pred, average=None)
+    print(classwise_f1)
+
+    ##### ACTION #####
+    action_true = [conversions[l] for l in all_labels['action']]
+    action_pred = [conversions[l] for l in all_labels['action_pred']]
+    classwise_f1 = f1_score(action_true, action_pred, average=None)
     print(classwise_f1)
 
 get_classwise_f1()
